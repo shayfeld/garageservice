6ab47a07fc60bf1a1c2e92bece2d1874aaec47cc
shayfeld <86830613+shayfeld@users.noreply.github.com>
2022-01-22 01:47:54 +0200

    tables without edit/delete

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
+
+const Treatment = require('../models/Treatment');
+const User = require('../models/User');
+
 // Welcome page
 router.get('/',(req, res)=>{
     res.render('welcome')
 });
 
-// Dashbord
+// Dashbord page
 router.get('/dashboard', ensureAuthenticated, (req, res)=>{
-    res.render('dashboard',{
-        name: req.user.name
+    User.find({}, (err, users) =>{
+        res.render('tables',{
+            userList: users
+        })
     })
+    
+});
+
+// New Treatment page
+router.get('/aboutUs', ensureAuthenticated, (req, res)=>{
+    res.render('aboutUs')
+});
+
+// New Treatment page
+router.get('/newTreatment', ensureAuthenticated, (req, res)=>{
+    res.render('newTreatment')
 });
 
 module.exports = router;
