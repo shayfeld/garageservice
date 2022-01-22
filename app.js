7ae41ffd0366814d0a182b0744842e8a0dc26c26
shayfeld <86830613+shayfeld@users.noreply.github.com>
2022-01-22 14:03:16 +0200

    mail verification
    mail verification for register

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use((req, res, next)=>{
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
